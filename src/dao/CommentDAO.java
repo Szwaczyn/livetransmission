bec283044191d613e6e746473cec2f43a37c3c3f
Michal Szwaczyk <michal.szwaczyk@outlook.com>
2020-04-17 22:15:44 +0200

    Implentation comment dao

--- src/dao/CommentDAO.java
+++ src/dao/CommentDAO.java
@@ -3,25 +3,45 @@ package dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import org.springframework.stereotype.Component;
 
 import domainmodel.Comment;
 import domainmodel.Match;
 import interfaces.ICommentDAO;
+import interfaces.IDatabase;
 
+@Component
 public class CommentDAO implements ICommentDAO {
 
 	private EntityManager em;
-	
+
+	public CommentDAO(IDatabase db) {
+		this.em = db.getEntityManager();
+	}
+
 	@Override
 	public boolean addComment(Comment comment) {
-		// TODO Auto-generated method stub
-		return false;
+		EntityTransaction et = this.em.getTransaction();
+		try {
+			et.begin();
+			em.persist(comment);
+			et.commit();
+			return true;
+		} catch (Exception e) {
+			et.rollback();
+			return false;
+		}
 	}
 
 	@Override
 	public List<Comment> getComment(Match match) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Comment> comments = em.createQuery("SELECT c FROM Comment c WHERE match = :match")
+				.setParameter("match", match)
+				.getResultList();
+		
+		return comments;
 	}
 
 }
